82e54b9f02d4dc1918dd10ef8095262913ecd04a
kaconant <krissyconant@gmail.com>
2018-07-21 12:53:13 -0400

    first commit

---
+++ src/essentialOils.js
@@ -0,0 +1,542 @@
+const oils = {
+    basil: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    bergamont: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    cajuput: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    cinnamon: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    clarySage: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    coriander: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    eucalyptus: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    grapefruit: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    hyssop: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    lemon: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    lemongrass: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    lime: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    mandarin: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    neroli: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    orange: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    peppermint: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    petitgrain: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    sage: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    spearmint: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    tangerine: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    teaTree: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    thyme: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    verbena: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: top,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    bay: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    blackPepper: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    cardamom: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    cypress: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    fennel: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    geranium: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    hyssop: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    juniper: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    lavender: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    majoram: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    melissa: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    nutmeg: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    palmaRosa: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    pine: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    rosemary: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    spikenard: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    yarrow: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: middle,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    cassia: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    cedarwood: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    clove: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    frankincense: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    ginger: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    jasmine: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    myrrh: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    patchouli: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    rose: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    rosewood: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    sandalwood: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    vanilla: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    vetiver: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    },
+    ylangYlang: { 
+        name: x,
+        desc: x,
+        medical: x,
+        mood: x,
+        level: base,
+        pregnant: null,
+        allergies: null,
+        kid: null
+    }
+}
