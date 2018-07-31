07334fb29cccfdbc504385e24da1b5c8ab37f123
kaconant <krissyconant@gmail.com>
2018-07-30 20:07:02 -0400

    added

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -7,7 +7,7 @@ class Jumbotron extends Component {
         <div>
             <div className="container-fluid msg-container">
               <div className="headline">
-                <h4>your oils. our recipes.</h4>  
+                <h4>our oils. your blends.</h4>  
                 <h6>what's your vibe today?</h6>
                 <div className="row flex-row justify-content-center buttons">
                   <a href="#base" onClick={this.props.handleMoodClick} className="transparent_btn"  data-target="#base" data-mood="calm">CALM</a>
@@ -20,17 +20,17 @@ class Jumbotron extends Component {
                 <div className="main-col">
                   <img src='./img/click2.png' height="40" />
                   <span className='info-header'>Choose</span>
-                  <span className="info-blurb">Whatever mood or needs you're feeling today, just choose it. Use the descriptions of each oil to guide you.</span>
+                  <span className="info-blurb">Pick a mood and follow the descriptions of each oil to guide you.</span>
                 </div>
                 <div className="main-col">
                   <img src='./img/fingerprint2.png' height="40" />
                   <span className='info-header'>Create</span>
-                  <span className="info-blurb">Once you've chosen a direction and your oils, we will create a blend that is as unique as you are.</span>
+                  <span className="info-blurb">After your selection, we will provide you the exact measurements for your blend.</span>
                 </div>
                 <div className="main-col">
                   <img src='./img/love2.png' height="40" />
                   <span className='info-header'>Enjoy</span>
-                  <span className="info-blurb">After you've mixed your oils, grab your crystals, and enjoy your unique blend. Namaste!</span>
+                  <span className="info-blurb">Sit back, relax, and enjoy a blend as unique as you are.</span>
                 </div>
               </div>    
             </div>
